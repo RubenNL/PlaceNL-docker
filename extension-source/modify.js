1da0773860e24d715e7dcff68f901d65ad3e56ef
Ruben <ruben@rubend.nl>
2023-07-20 18:46:56 +0200

    Send brand every minute instead of only once.

--- extension-source/modify.js
+++ extension-source/modify.js
@@ -2,11 +2,16 @@ setTimeout(()=>{
 	var th = document.getElementsByTagName('body')[0];
 	var s = document.createElement('script');
 	s.setAttribute('type', 'text/javascript');
-	s.innerHTML=`window.PLACENL_USERSCRIPT_CLIENT.ws.sendPayload('brand', {
-			author: 'RubenNL',
-			name: 'HeadlessFirefox',
-			version: document.querySelector(".toastify.toastify-right.toastify-bottom").innerText.match(/\(version (.*)\)/)[1].split(' ')[1].split('-')[0]
-		});
+	s.innerHTML=`
+		function updateBrand() {
+			window.PLACENL_USERSCRIPT_CLIENT.ws.sendPayload('brand', {
+				author: 'RubenNL',
+				name: 'HeadlessFirefox',
+				version: document.querySelector(".toastify.toastify-right.toastify-bottom").innerText.match(/\(version (.*)\)/)[1].split(' ')[1].split('-')[0]
+			});
+		}
+		updateBrand()
+		setInterval(updateBrand, 60*1000)
 	`
 	th.appendChild(s);
 }, 10000)
